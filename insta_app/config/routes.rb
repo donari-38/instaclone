4dd3a197941db05d83f102d2c3c3f263c119495d
EC2 Default User <ec2-user@ip-172-31-91-205.ec2.internal>
2020-01-12 03:59:58 +0000

    add user controller

--- insta_app/config/routes.rb
+++ insta_app/config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'users/new'
+
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
