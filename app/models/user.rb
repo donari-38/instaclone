ede52b20cc6472690aca05ed2cb541a4ddea0fa6
EC2 Default User <ec2-user@ip-172-31-91-205.ec2.internal>
2020-02-13 15:36:35 +0000

    コメント画面の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments
   before_save { self.email = email.downcase }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
   root 'static_pages#home'
-  # root 'posts#show'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
@@ -13,8 +12,9 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show]
   resources :posts, only: %i[index show create] do
-    resources :comments, only: %i[create]
+    resources :comments, only: [:create]
   end
+  
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
     get 'sign_out', to: 'users/sessions#destroy'
