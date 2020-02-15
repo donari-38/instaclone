062066ad3ad226476d4211f3bf24b1ca5a521a1f
EC2 Default User <ec2-user@ip-172-31-91-205.ec2.internal>
2020-02-15 14:58:31 +0000

    画像サイズの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,14 @@ class Post < ApplicationRecord
   mount_uploader :picture, PictureUploader
   belongs_to :user
   has_many :comments, dependent: :destroy
+  
+  validates :user_id, presence:true
+  validates :picture, presence:true
+  validate :picture_size
+  
+  private
+  
+  def picture_size
+    errors.add(:picture, 'should be less than 10MB')
+  end
 end
