38751555a57d78987d6d3c924d4594ee208e516d
EC2 Default User <ec2-user@ip-172-31-91-205.ec2.internal>
2020-02-29 07:30:41 +0000

    dbコメント

--- db/migrate/20200229033233_remove_name_from_users.rb
+++ db/migrate/20200229033233_remove_name_from_users.rb
@@ -1,9 +1,9 @@
-class RemoveNameFromUsers < ActiveRecord::Migration[5.1]
-  def up
-    remove_column :users, :name
-  end
+# class RemoveNameFromUsers < ActiveRecord::Migration[5.1]
+#   def up
+#     remove_column :users, :name
+#   end
   
-  def down
-    add_column :users, :name, :string
-  end
-end
+#   def down
+#     add_column :users, :name, :string
+#   end
+# end
