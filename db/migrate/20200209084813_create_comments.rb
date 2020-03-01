817c0958b14f51513dd8819b0900a3ad3a4023ad
EC2 Default User <ec2-user@ip-172-31-91-205.ec2.internal>
2020-03-01 03:16:30 +0000

    dbコメント

--- db/migrate/20200209084813_create_comments.rb
+++ db/migrate/20200209084813_create_comments.rb
@@ -3,11 +3,11 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      # t.string :content
-      # t.references :user, foreign_key: true
-      # t.references :post, foreign_key: true
+      t.string :content
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
 
-      # t.timestamps
+      t.timestamps
     end
   end
 end
